d97fc333de131ef18552e2dfd740d44d3a4717fd
Extirpater <edwardbzhang@gmail.com>
2024-04-12 11:11:57 -0400

    final

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
 import autoprefixer from 'autoprefixer';
-
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
-    ],
+        eslint()],
     css: {
       postcss: {
         plugins: [
