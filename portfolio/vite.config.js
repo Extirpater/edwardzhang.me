98cf060cede71703119816fc001bb6ab2dac2033
Extirpater <edwardbzhang@gmail.com>
2024-06-23 12:15:03 -0400

    added stuff

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/edwardzhang.me/",
+  base: './', // This ensures relative paths are used for assets
+
   plugins: [react()],
 })
