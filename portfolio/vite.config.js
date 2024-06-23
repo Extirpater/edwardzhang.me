8f2cbbd109cbbd698c100fe4442e3444c26c122b
Extirpater <edwardbzhang@gmail.com>
2024-06-23 12:32:55 -0400

    fix something?

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // This ensures relative paths are used for assets
+  base: '.', // This ensures relative paths are used for assets
 
   plugins: [react()],
 })
