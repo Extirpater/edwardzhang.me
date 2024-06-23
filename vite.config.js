35d893b8dc480cf60cd6e4fd07201afd6094e9a2
Extirpater <edwardbzhang@gmail.com>
2024-06-23 13:01:51 -0400

    scre gh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/', // This ensures relative paths are used for assets
-
   plugins: [react()],
 })
